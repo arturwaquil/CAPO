cbcb4f98b929abeb5de8e69189b639933249493e
Artur <arturwaquil@gmail.com>
2019-12-02 06:01:10 -0300

    funcionando :)

--- corretor.py
+++ corretor.py
@@ -1,11 +1,6 @@
 import numpy as np
 import cv2
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument('-n', '--numFotos', type=int,  help='Quantidade de fotos para corrigir', default=6)
-parser.add_argument('-e', '--extensao', type=str,  help='Extensao das fotos (sem o ponto)', default='jpg')
-args = parser.parse_args()
+import sys, os
 
 
 def window_is_open(windowname):
@@ -33,20 +28,38 @@ def is_valid_contour(c):
 	else:
 		return False
 
-def desenha_contornos(contours, imgOrig):
+def desenha_contornos(contours, imgOrig, cor=(255,0,0)):
 	img = imgOrig.copy()
 	for c in contours:
 		x, y, w, h = cv2.boundingRect(c)
-		cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 3)
+		cv2.rectangle(img, (x,y), (x+w,y+h), cor, 3)
 	return img
 
-def detecta_contornos(imgOrig, writename=None):
+def dimensoes_tabela(contours):
+
+	temp = contours.copy()
+	# ordena pelo x
+	temp.sort(key=lambda c:cv2.boundingRect(c)[0])
+
+	leftmost = temp[0]
+	x_leftmost, _, w_leftmost, _ = cv2.boundingRect(leftmost)
+
+	num_linhas = 0
+	for c in contours:
+		x,_,_,_ = cv2.boundingRect(c)
+		if x < x_leftmost + w_leftmost/2:
+			num_linhas += 1
+
+	num_colunas = int(len(contours)/num_linhas)
+
+	return num_linhas, num_colunas
+
+
+def detecta_contornos(imgOrig):
 	imshow("original", imgOrig)
 	imgGray = cv2.cvtColor(imgOrig, cv2.COLOR_BGR2GRAY); imshow("grayscale", imgGray)
 	# limiarizacao adaptativa devido a variacao de iluminacao na imagem
 	imgThres = cv2.adaptiveThreshold(imgGray,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY_INV,41,2); imshow("threshold", imgThres)
-	if writename is not None:
-		cv2.imwrite('threshold/' + writename, imgThres)
 
 	vert_kernel_length = 5#imgThres.shape[0]//50
 	hori_kernel_length = 5#imgThres.shape[1]//50
@@ -56,29 +69,27 @@ def detecta_contornos(imgOrig, writename=None):
 	temp = cv2.erode(imgThres, vert_kernel, iterations=3)
 	imgVertLines = cv2.dilate(temp, vert_kernel, iterations=3)
 	imshow("vert", imgVertLines)
+	cv2.imwrite('vert.jpg', imgVertLines)
 
 	# operacao morfologica para detectar retas horizontais na imagem, usando um kernel (hori_kernel_length x 1)
 	hori_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (hori_kernel_length, 1))
 	temp = cv2.erode(imgThres, hori_kernel, iterations=3)
 	imgHoriLines = cv2.dilate(temp, hori_kernel, iterations=3)
 	imshow("hori", imgHoriLines)
+	cv2.imwrite('hori.jpg', imgHoriLines)
 
 	# junta as verticais e horizontais
 	kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
 	imgTable = imgVertLines + imgHoriLines
 	imgTable = cv2.erode(~imgTable, kernel, iterations=2)
 	_, imgTable = cv2.threshold(imgTable, 128, 255, cv2.THRESH_BINARY_INV); imshow("table", imgTable)
-	if writename is not None:
-		cv2.imwrite('table/' + writename, imgTable)
+	cv2.imwrite('table.jpg', imgTable)
 
 	contours, _ = cv2.findContours(imgTable, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
 	# imprime os contornos detectados na imagem
 	imgFirstContours = desenha_contornos(contours, imgOrig)
 	imshow("first contours", imgFirstContours)
-
-	if writename is not None:
-		cv2.imwrite("firstcontours/" + writename, imgFirstContours)
 
 	# elimina contornos invalidos (i.e. muito pequenos ou de lados muito distintos)
 	tempContours = []
@@ -109,9 +120,6 @@ def detecta_contornos(imgOrig, writename=None):
 	imgContours = desenha_contornos(contours, imgOrig)
 	imshow("contours", imgContours)
 
-	if writename is not None:
-		cv2.imwrite("contours/" + writename, imgContours)
-
 	return imgContours, contours
 
 def monta_tabela(contours):
@@ -135,48 +143,19 @@ def monta_tabela(contours):
 		tabela.append(col)
 	tabela = tabela[1:]
 
-	# inverte ordem de linhas e colunas
-	# newTabela = []
-	# for i in range(num_linhas):
-	# 	linha = []
-	# 	for j in range(num_colunas):
-	# 		linha.append(tabela[j][i])
-	# 	newTabela.append(linha)
-	# tabela = newTabela
-
 	return tabela
-
-def dimensoes_tabela(contours):
-
-	temp = contours.copy()
-	# ordena pelo x
-	temp.sort(key=lambda c:cv2.boundingRect(c)[0])
-
-	leftmost = temp[0]
-	x_leftmost, _, w_leftmost, _ = cv2.boundingRect(leftmost)
-
-	num_linhas = 0
-	for c in contours:
-		x,_,_,_ = cv2.boundingRect(c)
-		if x < x_leftmost + w_leftmost/2:
-			num_linhas += 1
-
-	num_colunas = int(len(contours)/num_linhas)
-
-	return num_linhas, num_colunas
 
 def detecta_respostas(filename):
 
 	img = cv2.imread(filename)
-	_, contours = detecta_contornos(img, writename=filename)
+	_, contours = detecta_contornos(img)
 
 	imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 	imgCanny = cv2.Canny(imgGray, 100, 200); imshow("canny", imgCanny)
 
-	num_linhas, num_colunas = dimensoes_tabela(contours)
-	num_linhas -= 1
-	num_colunas -= 1
 	tabela = monta_tabela(contours)
+	num_linhas = len(tabela[0])
+	num_colunas = len(tabela)
 
 	respostas = []
 	for coluna in range(num_colunas):
@@ -200,44 +179,64 @@ def detecta_respostas(filename):
 
 		respostas.append(resposta)
 
-	return respostas
+	return respostas, tabela
 
-def testa_imgs():
-	import sys, os
-	for root, _, files in os.walk('imgs/'):
-		for file in files:
-			print(file + ': ', end='')
-			testa('imgs/' + file)
+def marca_respostas(gabarito, resposta, tabela, img):
+	num_linhas = len(tabela[0])
+	num_colunas = len(tabela)
+
+	corretas = []
+	erradas = []
+	for questao in range(len(resposta)):
+		alt = resposta[questao]
+		if gabarito[questao] == resposta[questao]:
+			corretas.append(tabela[questao][alt])
+		else:
+			erradas.append(tabela[questao][alt])
+	imgRespostas = desenha_contornos(corretas, img, (0,255,0))
+	imgRespostas = desenha_contornos(erradas, imgRespostas, (0,0,255))
+	imshow("respostas", imgRespostas)
+	return imgRespostas
+
 
 def testa(filename):
 	try:
-		gabarito = detecta_respostas(filename)
+		gabarito, _ = detecta_respostas(filename)
 		print(gabarito)
 	except:
-		print('erro no arquivo ' + filename)
+		print('erro no teste do arquivo ' + filename)
 
-if __name__ == "__main__":
-	nomeGab = 'teste/' + 'gabarito.' + args.extensao
-	nomeAluno = 'teste/' + 'aluno'
+def corrige_alunos():
 
-	gabarito = detecta_respostas(nomeGab)
-	respostas = []
+	# numero de alunos eh o numero de arquivos na pasta de provas
+	for _, _, files in os.walk('teste/provas/'):
+		num_alunos = len(files)
 
-	for i in range(args.numFotos):
-		nome = nomeAluno + str(i+1) + '.' + args.extensao
-		respostas.append(detecta_respostas(nome))
-		
-	for aluno in range(len(respostas)):
-		resp = respostas[aluno]
-		acertos = 0
+	dir_provas = 'teste/provas/'
+	dir_correcoes = 'teste/correcoes/'
+	extensao = 'jpg'
+
+	gabarito, _ = detecta_respostas('teste/gabarito.' + extensao)
+
+	for aluno in range(num_alunos):
+		nome = 'aluno' + str(aluno+1) + '.' + extensao
 		print('\nAluno ' + str(aluno+1) + ':')
 		
-		for questao in range(len(resp)):
-			if gabarito[questao] == resp[questao]:
+		resposta, tabela = detecta_respostas(dir_provas+nome)
+		
+		acertos = 0
+		for questao in range(len(resposta)):
+			if gabarito[questao] == resposta[questao]:
 				resultado = 'correto'
 				acertos += 1
 			else:
 				resultado = 'errado'
 			print('Questao ' + str(questao+1) + ': ' + resultado)	
 		
-		print('Nota: ' + str(acertos) + '/' + str(len(resp)))
+		print('Nota: ' + str(acertos) + '/' + str(len(resposta)))
+
+		imgRespostas = marca_respostas(gabarito, resposta, tabela, cv2.imread(dir_provas+nome))
+		cv2.imwrite(dir_correcoes + nome, imgRespostas)
+
+if __name__ == "__main__":
+	corrige_alunos()
